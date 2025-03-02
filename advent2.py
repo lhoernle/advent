24fc8c9a56cbb50caa7150f27d8bd091cd725e59
Lars Hoernlein <lhoernle@gmail.com>
2025-03-02 08:51:04 -0800

    final product

--- advent2.py
+++ advent2.py
@@ -35,16 +35,6 @@ def left_over_data(data):
             new_data.append(row)
     return new_data
 
-test_data = [
-                [7, 6, 4, 2, 1],
-                [1, 2, 7, 8, 9],
-                [9, 7, 6, 2, 1],
-                [1, 3, 2, 4, 5],
-                [8, 6, 4, 4, 1],
-                [1, 3, 6, 7, 9]
-
-]
-
 if __name__ == "__main__":
     filename = "data2.txt"
     data = read_data(filename)
