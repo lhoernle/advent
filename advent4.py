b91ed39184c87c9339368acdd8f44d36c053ac9d
Lars Hoernlein <lhoernle@gmail.com>
2025-03-19 09:13:30 -0700

    Just updating

--- advent4.py
+++ advent4.py
@@ -5,13 +5,20 @@ def read_data(filename):
         data = file.readlines()
     return data
 
-lines = read_data('test4.txt')
+lines = read_data('data4.txt')
 
 char_map = defaultdict(set)
 for r, row in enumerate(lines):
     for c, val in enumerate(row):
         char_map[val].add((r, c))
 
+total = 0
 for r, c in char_map['X']:
     for dr, dc in [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]:
-        print(dr, dc)
+        for i, char in enumerate('XMAS'):
+            if (r + dr*i, c + dc*i) not in char_map[char]:
+                break
+        else:
+            total += 1
+
+print(total)
