fba03cf5ba4bd73adb8e93883ab2dd91e57f6fd5
Lars Hoernlein <lhoernle@gmail.com>
2025-03-27 23:28:36 -0700

    work in progress

--- advent4.py
+++ advent4.py
@@ -5,20 +5,32 @@ def read_data(filename):
         data = file.readlines()
     return data
 
-lines = read_data('data4.txt')
+lines = read_data('test4.txt')
 
 char_map = defaultdict(set)
 for r, row in enumerate(lines):
     for c, val in enumerate(row):
         char_map[val].add((r, c))
 
-total = 0
+part1 = 0
 for r, c in char_map['X']:
     for dr, dc in [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]:
         for i, char in enumerate('XMAS'):
             if (r + dr*i, c + dc*i) not in char_map[char]:
                 break
         else:
-            total += 1
+            part1 += 1
 
-print(total)
+upleft = lambda r, c: (r - 1, c - 1)
+upright = lambda r, c: (r - 1, c + 1)
+downleft = lambda r, c: (r + 1, c - 1)
+downright = lambda r, c: (r + 1, c + 1)
+
+part2 = 0
+print(char_map)
+for r, c in char_map["A"]:
+    if upleft(r, c) in char_map["M"]:
+        if downleft(r, c) in char_map["M"] and upright(r, c)
+
+
+print(f'Part 1: {part1}')
